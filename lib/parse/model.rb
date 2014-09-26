3064b935849a915fe5a2b8a3fb2448a0ebff29d9
Chris Bielinski <chris@shadow.io>
2014-09-26 14:29:34 -0700

    add #find_by magic on Parse::Model

--- lib/parse/model.rb
+++ lib/parse/model.rb
@@ -7,7 +7,19 @@ module Parse
     end
 
     def self.find(object_id)
-      self.new Parse::Query.new(self.to_s).eq('objectId',object_id).first
+      find_by_object_id(object_id)
+    end
+
+    def self.find_by(query_hash)
+      self.new Parse::Query.new(self.to_s).eq(query_hash).first
+    end
+
+    def self.method_missing(m, *args, &block)
+      if m.to_s.starts_with?('find_by_')
+        raise 'unknown find_by signature' unless args.length == 1
+        return find_by m.to_s.gsub('find_by_','').camelize(:lower) => args[0]
+      end
+      super
     end
 
   end
