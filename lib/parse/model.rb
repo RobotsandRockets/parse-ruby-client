7a0fdb8565c1ceb5606a69a4ab2efaa2b28a20ec
Chris Bierlinski <chris@shadow.io>
2015-04-14 09:36:27 -0700

    fix typo; bump version

--- lib/parse/model.rb
+++ lib/parse/model.rb
@@ -44,7 +44,7 @@ module Parse
     end
 
     def model_name
-      return ActiveModel::Name.new(self.class) if defined?(ACtiveModel::Name)
+      return ActiveModel::Name.new(self.class) if defined?(ActiveModel::Name)
       self.class.parse_object_name.gsub('_','').underscore
     end
 
